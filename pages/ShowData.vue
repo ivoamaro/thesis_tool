2cea7b802f307e485ba901b4b21b47bbfffd1063
ivoamaro <33934913+ivoamaro@users.noreply.github.com>
2020-04-13 11:24:18 +0100

    v5.5.2

--- pages/ShowData.vue
+++ pages/ShowData.vue
@@ -4,6 +4,18 @@
       <h1 class="title is-1">Database</h1>
       <h2 class="subtitle is-2">List of all entrys</h2>
     </div>
+    <div class="column">
+      <label class="label">Retrieved On</label>
+
+      <div class="select">
+        <select v-model="search">
+          <option>All</option>
+          <option>Survey</option>
+          <option>Online</option>
+          <option>Professional</option>
+        </select>
+      </div>
+    </div>
     <div v-for="(t,i) in setData" :key="t+i">
       <nuxt-link :to="{ 
       name:'Id',
@@ -24,12 +36,30 @@ import { mapState } from "vuex";
 export default {
   data() {
     return {
-      search: ""
+      search: "All"
     };
   },
   computed: {
     setData() {
-      return this.$store.state.posts;
+      switch (this.search) {
+        case "Online":
+          return this.$store.state.posts.filter(fil => {
+            return fil.found.match("Online");
+          });
+          break;
+        case "Survey":
+          return this.$store.state.posts.filter(fil => {
+            return fil.found.match("Survey");
+          });
+          break;
+        case "Professional":
+          return this.$store.state.posts.filter(fil => {
+            return fil.found.match("Professional");
+          });
+          break;
+        default:
+          return this.$store.state.posts;
+      }
     },
     ...mapState(["posts"])
   }
